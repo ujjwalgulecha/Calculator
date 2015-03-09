82b09214771b9d3db0f936c1dca2395dd9ac5c97
Ujjwal Gulecha <ujjwal.gulecha@gmail.com>
2015-03-09 22:20:57 +0530

    Completely Changed the UI and the Logic of the app. Will modify further

--- app/src/main/java/com/ujjwalgulecha/calculator/MainActivity.java
+++ app/src/main/java/com/ujjwalgulecha/calculator/MainActivity.java
@@ -6,15 +6,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
+    public String firstValue=" ";
+    public int flag=1;
 
-    Button addButton,subButton,mulButton,divButton;
     TextView answer_display;
     EditText v1,v2;
 
@@ -23,13 +22,10 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addButton = (Button) findViewById(R.id.add_button);
-        subButton = (Button) findViewById(R.id.sub_button);
-        mulButton = (Button) findViewById(R.id.mul_button);
-        divButton = (Button) findViewById(R.id.div_button);
+
         answer_display = (TextView) findViewById(R.id.result_display);
         v1 = (EditText) findViewById(R.id.value1);
-        v2 = (EditText) findViewById(R.id.value2);
+
     }
 
     @Override
@@ -60,101 +56,338 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void Add(View view) {
-
-        String val1 = v1.getText().toString();
-        String val2 = v2.getText().toString();
-        if(val1.matches("-?\\d+(\\.\\d+)?")==false || val2.matches("-?\\d+(\\.\\d+)?")==false)
+    public void clickSeven(View view) {
+        if(flag==1) {
+            firstValue = "7";
+            flag=0;
+        }
+        else
         {
-            Toast.makeText(getApplicationContext(),"Numbers Only!",Toast.LENGTH_SHORT).show();
+            firstValue  = firstValue + "7";
         }
-        else {
-            if (val1.length() == 0 && val2.length() == 0) {
-                answer_display.setText(" ");
-            } else if (val1.length() == 0) {
-                answer_display.setText(val2);
-            } else if (val2.length() == 0) {
-                answer_display.setText(val1);
-            } else {
-                double v1 = Double.parseDouble(val1);
-                double v2 = Double.parseDouble(val2);
-                double res = v1 + v2;
-                String ans = Double.toString(res);
-                answer_display.setText(ans);
-            }
-        }
+
+        v1.setText(firstValue);
+        v1.setSelection(v1.getText().length());
     }
 
-    public void Sub(View view) {
-        String val1 = v1.getText().toString().trim();
-        String val2 = v2.getText().toString().trim();
-        if(val1.matches("-?\\d+(\\.\\d+)?")==false || val2.matches("-?\\d+(\\.\\d+)?")==false)
+    public void clickEight(View view) {
+        if(flag==1) {
+            firstValue = "8";
+            flag=0;
+        }
+        else
         {
-            Toast.makeText(getApplicationContext(), "Numbers Only!", Toast.LENGTH_SHORT).show();
+            firstValue  = firstValue + "8";
         }
-        else {
-            if (val1.length() == 0 && val2.length() == 0) {
-                answer_display.setText(" ");
-            } else if (val1.length() == 0) {
-                answer_display.setText(val2);
-            } else if (val2.length() == 0) {
-                answer_display.setText(val1);
-            } else {
-                double v1 = Double.parseDouble(val1);
-                double v2 = Double.parseDouble(val2);
-                double res = v1 - v2;
-                String ans = Double.toString(res);
-                answer_display.setText(ans);
-            }
-        }
+
+        v1.setText(firstValue);
+        v1.setSelection(v1.getText().length());
     }
 
-    public void Mul(View view) {
-        String val1 = v1.getText().toString().trim();
-        String val2 = v2.getText().toString().trim();
-        if(val1.matches("-?\\d+(\\.\\d+)?")==false || val2.matches("-?\\d+(\\.\\d+)?")==false)
+    public void clickNine(View view) {
+        if(flag==1) {
+            firstValue = "9";
+            flag=0;
+        }
+        else
         {
-            Toast.makeText(getApplicationContext(), "Numbers Only!", Toast.LENGTH_SHORT).show();
+            firstValue  = firstValue + "9";
         }
-        else {
-            if (val1.length() == 0 && val2.length() == 0) {
-                answer_display.setText(" ");
-            } else if (val1.length() == 0) {
-                answer_display.setText(val2);
-            } else if (val2.length() == 0) {
-                answer_display.setText(val1);
-            } else {
-                Double v1 = Double.parseDouble(val1);
-                Double v2 = Double.parseDouble(val2);
-                Double res = v1 * v2;
-                String ans = Double.toString(res);
-                answer_display.setText(ans);
-            }
-        }
+
+        v1.setText(firstValue);
+        v1.setSelection(v1.getText().length());
     }
 
-    public void Div(View view) {
-
-        String val1 = v1.getText().toString().trim();
-        String val2 = v2.getText().toString().trim();
-        if(val1.matches("-?\\d+(\\.\\d+)?")==false || val2.matches("-?\\d+(\\.\\d+)?")==false)
-        {
-            Toast.makeText(getApplicationContext(), "Numbers Only!", Toast.LENGTH_SHORT).show();
+    public void clickFour(View view) {
+        if(flag==1) {
+            firstValue = "4";
+            flag=0;
         }
-        else {
-            if (val1.length() == 0 && val2.length() == 0) {
-                answer_display.setText("  ");
-            } else if (val1.length() == 0) {
-                answer_display.setText(val2);
-            } else if (val2.length() == 0) {
-                answer_display.setText(val1);
-            } else {
-                double v1 = Double.parseDouble(val1);
-                double v2 = Double.parseDouble(val2);
-                double res = v1 / v2;
-                String ans = Double.toString(res);
-                answer_display.setText(ans);
+        else
+        {
+            firstValue  = firstValue + "4";
+        }
+
+        v1.setText(firstValue);
+        v1.setSelection(v1.getText().length());
+    }
+
+    public void clickFive(View view) {
+        if(flag==1) {
+            firstValue = "5";
+            flag=0;
+        }
+        else
+        {
+            firstValue  = firstValue + "5";
+        }
+
+        v1.setText(firstValue);
+        v1.setSelection(v1.getText().length());
+    }
+
+    public void clickSix(View view) {
+        if(flag==1) {
+            firstValue = "6";
+            flag=0;
+        }
+        else
+        {
+            firstValue  = firstValue + "6";
+        }
+
+        v1.setText(firstValue);
+        v1.setSelection(v1.getText().length());
+    }
+
+    public void clickOne(View view) {
+        if(flag==1) {
+            firstValue = "1";
+            flag=0;
+        }
+        else
+        {
+            firstValue  = firstValue + "1";
+        }
+
+        v1.setText(firstValue);
+        v1.setSelection(v1.getText().length());
+    }
+
+    public void clickTwo(View view) {
+        if(flag==1) {
+            firstValue = "2";
+            flag=0;
+        }
+        else
+        {
+            firstValue  = firstValue + "2";
+        }
+
+        v1.setText(firstValue);
+        v1.setSelection(v1.getText().length());
+    }
+
+    public void clickThree(View view) {
+        if(flag==1) {
+            firstValue = "3";
+            flag=0;
+        }
+        else
+        {
+            firstValue  = firstValue + "3";
+        }
+
+        v1.setText(firstValue);
+        v1.setSelection(v1.getText().length());
+    }
+
+    public void clickZero(View view) {
+        if(flag==1) {
+            firstValue = "0";
+            flag=0;
+        }
+        else
+        {
+            firstValue  = firstValue + "0";
+        }
+
+        v1.setText(firstValue);
+        v1.setSelection(v1.getText().length());
+    }
+
+    public void clickDot(View view) {
+        if(flag==1) {
+            firstValue = ".";
+            flag=0;
+        }
+        else
+        {
+            firstValue  = firstValue + ".";
+        }
+
+        v1.setText(firstValue);
+        v1.setSelection(v1.getText().length());
+    }
+
+    public void clickClear(View view) {
+        firstValue  = " ";
+        flag=1;
+        v1.setText(firstValue);
+        answer_display.setText(" ");
+
+    }
+
+    public void clickBack(View view) {
+        if(firstValue.length()==0)
+        {
+            v1.setText(" ");
+            flag=1;
+        }
+        else
+        {
+            String abc = firstValue.substring(0,firstValue.length()-1);
+            firstValue = abc;
+            v1.setText(firstValue);
+            v1.setSelection(v1.getText().length());
+        }
+
+    }
+
+    public void clickEqual(View view) {
+
+        Double answer = evaluate(firstValue);
+        String a = Double.toString(answer);
+        answer_display.setText(a);
+
+    }
+    public static double evaluate(final String str) {
+        class Parser {
+            int pos = -1, c;
+
+            void eatChar() {
+                c = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            void eatSpace() {
+                while (Character.isWhitespace(c)) eatChar();
+            }
+
+            double parse() {
+                eatChar();
+                double v = parseExpression();
+                if (c != -1) throw new RuntimeException("Unexpected: " + (char)c);
+                return v;
+            }
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor | term brackets
+            // factor = brackets | number | factor `^` factor
+            // brackets = `(` expression `)`
+
+            double parseExpression() {
+                double v = parseTerm();
+                for (;;) {
+                    eatSpace();
+                    if (c == '+') { // addition
+                        eatChar();
+                        v += parseTerm();
+                    } else if (c == '-') { // subtraction
+                        eatChar();
+                        v -= parseTerm();
+                    } else {
+                        return v;
+                    }
+                }
+            }
+
+            double parseTerm() {
+                double v = parseFactor();
+                for (;;) {
+                    eatSpace();
+                    if (c == '/') { // division
+                        eatChar();
+                        v /= parseFactor();
+                    } else if (c == '*' || c == '(') { // multiplication
+                        if (c == '*') eatChar();
+                        v *= parseFactor();
+                    } else {
+                        return v;
+                    }
+                }
+            }
+
+            double parseFactor() {
+                double v;
+                boolean negate = false;
+                eatSpace();
+                if (c == '(') { // brackets
+                    eatChar();
+                    v = parseExpression();
+                    if (c == ')') eatChar();
+                } else { // numbers
+                    if (c == '+' || c == '-') { // unary plus & minus
+                        negate = c == '-';
+                        eatChar();
+                        eatSpace();
+                    }
+                    StringBuilder sb = new StringBuilder();
+                    while ((c >= '0' && c <= '9') || c == '.') {
+                        sb.append((char)c);
+                        eatChar();
+                    }
+                    if (sb.length() == 0) throw new RuntimeException("Unexpected: " + (char)c);
+                    v = Double.parseDouble(sb.toString());
+                }
+                eatSpace();
+                if (c == '^') { // exponentiation
+                    eatChar();
+                    v = Math.pow(v, parseFactor());
+                }
+                if (negate) v = -v; // exponentiation has higher priority than unary minus: -3^2=-9
+                return v;
             }
         }
+        return new Parser().parse();
     }
+
+    public void clickAdd(View view) {
+        if(flag==1) {
+            firstValue = "+";
+            flag=0;
+        }
+        else
+        {
+            firstValue  = firstValue + "+";
+        }
+
+        v1.setText(firstValue);
+        v1.setSelection(v1.getText().length());
+
+    }
+    public void clickSubtract(View view) {
+        if(flag==1) {
+            firstValue = "-";
+            flag=0;
+        }
+        else
+        {
+            firstValue  = firstValue + "-";
+        }
+
+        v1.setText(firstValue);
+        v1.setSelection(v1.getText().length());
+
+    }
+    public void clickMultiply(View view) {
+        if(flag==1) {
+            firstValue = "*";
+            flag=0;
+        }
+        else
+        {
+            firstValue  = firstValue + "*";
+        }
+
+        v1.setText(firstValue);
+        v1.setSelection(v1.getText().length());
+
+    }
+
+    public void clickDivide(View view) {
+        if(flag==1) {
+            firstValue = "/";
+            flag=0;
+        }
+        else
+        {
+            firstValue  = firstValue + "/";
+        }
+
+        v1.setText(firstValue);
+        v1.setSelection(v1.getText().length());
+
+    }
+
 }
